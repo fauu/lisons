804b5156ec9a32412ea5b5cc0046fad0187bae3c
fauu <fau999@gmail.com>
2018-04-28 18:17:00 +0200

    Use new hot-reloading method, this time for real

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,16 +1,12 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import { AppContainer } from "react-hot-loader"
 
 import { App } from "~/app/components"
 
-const rootEl = document.getElementById("app")
 ReactDOM.render(
   // @ts-ignore
-  <AppContainer>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </AppContainer>,
-  rootEl
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("app")
 )
